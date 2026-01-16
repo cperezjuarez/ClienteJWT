ec70d31be828f4b7efe3b480ede1505f9f658f04
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-16 17:14:27 +0100

    Add: Implementación de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './components/auth/login/login';
+import { UserList } from './components/users/user-list/user-list';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full'},
+    { path: 'login', component: Login },
+    { path: 'users-list', component: UserList, canActivate: [authGuard] }
+];
---
+++ src/app/components/auth/login/login.ts
@@ -0,0 +1,45 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit, signal } from '@angular/core';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginRequest } from '../../../models';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login implements OnInit {
+  loading = signal(false);
+  error = signal<HttpErrorResponse | null>(null);
+  form!: FormGroup;
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl(null, [Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+    })
+  }
+
+  onSubmit(): void {
+    this.loading.set(true);
+    this.error.set(null);
+
+    const request: LoginRequest = {username: this.form.value.username, password: this.form.value.password}
+    
+    this.authService.login(request)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/users-list']);
+        },
+        error: (err) => {
+          this.error.set(err);
+          this.loading.set(false);
+        }
+      });
+  }
+}
