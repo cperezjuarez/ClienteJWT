7d00645c61a27398a7a1c879050db2b1856be866
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2026-01-21 12:37:06 +0100

    Add: Implementación de editar usuario

--- src/app/components/users/update-user-form/update-user-form.ts
+++ src/app/components/users/update-user-form/update-user-form.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-update-user-form',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './update-user-form.html',
   styleUrl: './update-user-form.css',
 })
 export class UpdateUserForm {
+  dialogRef = inject(MatDialogRef<UpdateUserForm>);
+  form!: FormGroup;
 
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      username: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      role: new FormControl('USER', [Validators.required]),
+      enabled: new FormControl(true)
+    })
+  }
+
+  save() {
+    if (this.form.valid) {
+      this.dialogRef.close(this.form.value);
+    }
+  }
+
+  cancel() {
+    this.dialogRef.close();
+  }
 }
