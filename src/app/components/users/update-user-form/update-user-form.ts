9d429d9a59c261ded54f49e92b211a396ff2be51
Cristian Pérez Juárez <cristianpj0808@gmail.com>
2026-01-21 12:06:03 +0100

    Create: Componente update-user-form

---
+++ src/app/components/users/update-user-form/update-user-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-user-form',
+  imports: [],
+  templateUrl: './update-user-form.html',
+  styleUrl: './update-user-form.css',
+})
+export class UpdateUserForm {
+
+}
