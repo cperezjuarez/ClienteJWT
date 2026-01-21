055e6402163fcd03f16c88827a354642952b15aa
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-01-21 16:30:00 +0100

    Delete: Eliminado el botón de añadir y sus funcionalidades del componente user-list

--- src/app/components/users/user-list/user-list.ts
+++ src/app/components/users/user-list/user-list.ts
@@ -9,10 +9,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { AuthService } from '../../../services/auth.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UpdateUserForm } from '../update-user-form/update-user-form';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-user-list',
-  imports: [CommonModule, MatDialogModule, MatButtonModule],
+  imports: [CommonModule, MatDialogModule, MatButtonModule, RouterLink],
   templateUrl: './user-list.html',
   styleUrl: './user-list.css',
 })
@@ -41,46 +42,6 @@ export class UserList implements OnInit {
       error: (err) => {
         this.error.set(err);
         this.loading.set(false);
-      }
-    });
-  }
-
-  // Crear usuario
-  createUser() {
-    // Modal
-    const dialogRef = this.dialog.open(AddUserForm, {
-      width: '400px',
-    });
-
-    // Resultado del modal
-    dialogRef.afterClosed().subscribe(result => {
-      if (result) {
-        this.loading.set(true);
-        this.error.set(null);
-
-        const request: RegisterRequest = { username: result.username, password: result.password, email: result.email }
-
-        // Service
-        this.authService.register(request).subscribe({
-          next: () => {
-            this.snackBar.open('Usuario creado correctamente', 'OK', {
-              duration: 3000,
-              horizontalPosition: 'center',
-              verticalPosition: 'top'
-            })
-            this.loadUsers();
-            this.loading.set(false);
-          },
-          error: (err) => {
-            this.snackBar.open('ERROR: Error al crear el usuario', 'OK', {
-              duration: 3000,
-              verticalPosition: 'top',
-              panelClass: ['error-snackbar']
-            })
-            this.error.set(err);
-            this.loading.set(false);
-          }
-        });
       }
     });
   }
